61ed3894ece6502c9944a0ddd7e9f0f18fa21fa6
howsilence <statton0104@gmail.com>
2022-01-14 14:49:29 -0500

    project finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const thisFuckinThing = document.querySelector("#new-task-description")
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault()
+    createEle(thisFuckinThing.value)
+  });})
+
+function createEle(toDo){
+  const liEle = document.createElement('li');
+  const subBut = document.createElement("button");
+  subBut.textContent = "x"
+  liEle.textContent = `${toDo} `;
+  document.querySelector("#tasks").appendChild(liEle);
+  subBut.addEventListener("click", (e)=>{
+    e.target.parentNode.remove();
+  })
+  liEle.appendChild(subBut);
+};
